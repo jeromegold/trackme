7dcd4ac9e8a12c2ee3bc3980f84a169309030355
Jerome Goldstein <jeromegold@gmail.com>
2018-07-18 20:06:04 +1000

    NEW: Finished task 1_3p with server.js and nodemon

--- web/public/app.js
+++ web/public/app.js
@@ -14,10 +14,14 @@ const user = $('#user').val();
 const name = $('#name').val();
 devices.push({ user, name });
 localStorage.setItem('devices', JSON.stringify(devices));
-location.href = 'device_list.html';
+location.href = '/';
 });
 
 $('#send-command').on('click', function() {
 const command = $('#command').val();
 console.log(`command is: ${command}`);
 });
+
+$('#navbar').load('navbar.html');
+
+$('#footer').load('footer.html');
