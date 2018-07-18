a093f8dcb2187c0e5b50eb6f23d43e0e3f51ad42
Jerome Goldstein <jeromegold@gmail.com>
2018-07-18 19:44:57 +1000

    FIX: removed node_modules

--- web/public/server.js
+++ web/public/server.js
@@ -5,10 +5,24 @@ const base = `${__dirname}/public`;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-res.send('hello world');
+app.get('/', function (req, res) {
+res.sendFile(`${base}/device_list.html`);
 });
 
 app.listen(port, () => {
 console.log(`listening on port ${port}`);
+});
+
+app.get('/register-device', (req, res) => {
+res.sendFile(`${base}/register_device.html`);
+});
+app.get('/send-command', (req, res) => {
+res.sendFile(`${base}/send_command.html`);
+});
+app.get('/about', (req, res) => {
+res.sendFile(`${base}/about_me.html`);
+});
+
+app.get('*', (req, res) => {
+res.sendFile(`${base}/404.html`);
 });
